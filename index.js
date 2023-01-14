8ae1dc391ff27fdcd92f0dbfd05bb333503b0010
ramyar omed ali <ro21090@auis.edu.krd>
2023-01-14 23:40:32 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(params, str) {
+    return params.filter((n) => {
+        return (n == str.toUpperCase() || n == str.toLowerCase() || n == str)
+    });
+}
+
+function fuzzyMatch(params, char) {
+    return params.filter((n) => {
+        return (n[0] == char[0]);
+    })
+}
+function matchName(params, namee) {
+    let x = [{}]
+    let i = 0;
+    for (const user of params) {
+        if (user.name == namee) {
+            x[i] = (user);
+            i++;
+        }
+    }
+    return x;
+}
